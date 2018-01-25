1654eb8dc2bb0fbd5a1683f14aa88336afcf34c4
Vladislav Korbut <v.korbut8@gmail.com>
2018-01-25 15:18:48 +0300

    Setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const webserver = require('gulp-webserver');
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+
+const src = {
+  scss: {
+    styles: './src/scss/styles.scss',
+    all: './src/scss/**/*.scss',
+  },
+  img: './src/img/**/*.*',
+};
+
+const dist = {
+  root: './dist',
+  css: './dist/css/',
+  img: './dist/img',
+};
+
+gulp.task('scss', function() {
+  return gulp
+    .src(src.scss.styles)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(dist.css));
+});
+
+gulp.task('img', function() {
+  gulp
+    .src(src.img)
+    .pipe(
+      imagemin([
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 })
+      ])
+    )
+    .pipe(gulp.dest(dist.img));
+});
+
+gulp.task('build', ['scss', 'img']);
+
+gulp.task('watch', function() {
+  gulp.watch(src.scss.styles, ['scss']);
+  gulp.watch(src.scss.all, ['scss']);
+  gulp.watch(src.img, ['img']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src(dist.root).pipe(
+    webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true,
+    })
+  );
+});
+
+gulp.task('default', ['build', 'watch', 'webserver']);
